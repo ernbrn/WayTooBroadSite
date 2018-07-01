3330e25a6a86ba7032917699bc8774414690b907
Erin Brown <emberbrown@gmail.com>
2018-07-01 13:45:21 -0400

    Adds back audio player, does not preload audio

--- src/Episode.js
+++ src/Episode.js
@@ -5,6 +5,16 @@ export default function Episode({ episode }) {
   return (
     <div className="episode">
       <div className="episode__title"><a href={episode.link} target="_blank">{episode.title.replace('- Way Too Broad', '')}</a></div>
+      {
+        episode.enclosure &&
+        <audio
+          src={episode.enclosure.url}
+          controls
+          preload="none"
+        >
+          You are using an outdated browser so you can't hear the audio you fool!
+        </audio>
+      }
       <div dangerouslySetInnerHTML={{ __html: episode.content }} />
     </div>
   )
