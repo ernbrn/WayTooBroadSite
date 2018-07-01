f5f7009a992b101650650950289ba4bbcd068993
Erin Brown <emberbrown@gmail.com>
2018-06-30 22:20:56 -0400

    First commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Welcome to Way Too Bread!</h1>
+        </header>
+        <p className="App-intro">
+          Is this bod bast about bread?
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
