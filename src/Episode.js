a48c6fa554f4eecb4399f765e448556f87295cc6
Erin Brown <emberbrown@gmail.com>
2018-07-01 13:26:59 -0400

    Remove episode players

--- src/Episode.js
+++ src/Episode.js
@@ -5,15 +5,6 @@ export default function Episode({ episode }) {
   return (
     <div className="episode">
       <div className="episode__title"><a href={episode.link} target="_blank">{episode.title.replace('- Way Too Broad', '')}</a></div>
-      {
-        episode.enclosure &&
-        <audio
-          src={episode.enclosure.url}
-          controls
-        >
-          You are using an outdated browser so you can't hear the audio you fool!
-        </audio>
-      }
       <div dangerouslySetInnerHTML={{ __html: episode.content }} />
     </div>
   )
