0437e96ab0e27b97810d6f78cfbbd2f93bf9a4d9
Erin Brown <emberbrown@gmail.com>
2018-07-01 00:37:53 -0400

    Be able to listen to the episodes inline, styles

--- src/Episode.js
+++ src/Episode.js
@@ -4,7 +4,16 @@ import './Episode.css';
 export default function Episode({ episode }) {
   return (
     <div className="episode">
-      <div className="episode__title">{episode.title}</div>
+      <div className="episode__title"><a href={episode.link} target="_blank">{episode.title.replace('- Way Too Broad', '')}</a></div>
+      {
+        episode.enclosure &&
+        <audio
+          src={episode.enclosure.url}
+          controls
+        >
+          You are using an outdated browser so you can't hear the audio you fool!
+        </audio>
+      }
       <div dangerouslySetInnerHTML={{ __html: episode.content }} />
     </div>
   )
