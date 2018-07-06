2eed7f31519031c73e6d490057718a635567b6ca
Erin Brown <emberbrown@gmail.com>
2018-07-05 23:21:36 -0400

    About page realness

--- src/About.js
+++ src/About.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import Nav from './Nav';
+import { HashLink as Link } from 'react-router-hash-link';
+import ben from './ben.png';
+import erin from './erin.PNG';
+import hannah from './hannah.jpg';
+import QAndA from './QAndA';
+import './About.css';
 
 export default function About() {
   return (
@@ -7,8 +13,68 @@ export default function About() {
       <header>
         <h1>About Us</h1>
       </header>
-      <h2>Who are these fools?</h2>
+      <h2>A new era that spreads each small wing and can not be reached with one small wing</h2>
       <Nav />
+      <div className="bio">
+        <div className="container">
+          <div className="bio__section">
+            <div><img className="pic" src={hannah} /></div>
+            <QAndA answers={hannahAnswers} />
+          </div>
+          <div className="bio__section">
+            <div><img className="pic" src={ben} /></div>
+            <QAndA answers={benAnswers} />
+          </div>
+          <div className="bio__section">
+            <div><img className="pic" src={erin} /></div>
+            <QAndA answers={erinAnswers} />
+          </div>
+
+        </div>
+      </div>
+      <div className="footer">Q&A Questions taken from the Google translated <a href="http://goosehouse.jp/profile/" target="_blank">Goose House website</a> and talked about on <Link to="/#32">this episode</Link></div>
     </div>
   )
 }
+
+const hannahAnswers = {
+  person: 'HANNAH',
+  instruments: 'French horn, trumpet, bad piano',
+  musicians: 'Janelle Monáe, Guster, Erin Brown',
+  movie: 'I saw Ladybird recently, that was pretty good',
+  book: 'Since Harry Potter is a series of books, I gotta say Ella Enchanted (MAKE IT A MINISERIES PLEASE)',
+  endOfLife: 'An entire key lime pie made with fresh key lime juice. No effing raspberry sauce',
+  tongue: 'All of it, I think',
+  love: 'Cats, Ian, a lot of other things you can hear about on the podcast!',
+  onga: 'None of your business',
+  whatAreYouDoing: 'This is a fucking trick question and I won’t be tricked',
+  aWord: 'LAUGHING!'
+};
+
+const benAnswers = {
+  person: 'BEN',
+  instruments: '',
+  musicians: '',
+  movie: '',
+  book: '',
+  endOfLife: '',
+  tongue: '',
+  love: '',
+  onga: '',
+  whatAreYouDoing: '',
+  aWord: ''
+};
+
+const erinAnswers = {
+  person: 'ERIN',
+  instruments: 'Guitar, piano, vocables',
+  musicians: 'I\'ve influenced a lot of musicians, but if I have to choose who I\'ve influenced the most I\'d say probably Janelle Monáe or Taylor Swift',
+  movie: 'Meet Joe Black and My Big Fat Greek Wedding. There was a summer that Hannah and I watched these fine films OVER AND OVER AND OVER.',
+  book: 'The Harry Potter series did a lot for Wizard visibility so I\'ll say those',
+  endOfLife: 'Something that I\'ve not just choked to death on',
+  tongue: 'This is a tough one (beef tongue joke)',
+  love: 'My wife, my dog, my family, my podcast co-hosts (who are also my family)',
+  onga: 'WHY NOT',
+  whatAreYouDoing: 'Probably recording this podcast.',
+  aWord: 'Cousins'
+};
---
+++ src/QAndA.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import './QAndA.css';
+
+export default function QAndA({ answers }) {
+  return (
+    <div className="qa">
+      <h2 className="name">{answers.person}</h2>
+      <p>
+        <span className="qa__q">What instruments can you do? /</span> {answers.instruments}
+      </p>
+      <p>
+        <span className="qa__q">Who are the musicians influenced? /</span> {answers.musicians}
+      </p>
+      <p>
+        <span className="qa__q">What is your important movie? /</span> {answers.movie}
+      </p>
+      <p>
+        <span className="qa__q">What is the important book? /</span> {answers.book}
+      </p>
+      <p>
+        <span className="qa__q">What do you want to eat at the end in your life? /</span> {answers.endOfLife}
+      </p>
+      <p>
+        <span className="qa__q">What is not good at beef tongue ? /</span> {answers.tongue}
+      </p>
+      <p>
+        <span className="qa__q">What do you love? /</span> {answers.love}
+      </p>
+      <p>
+        <span className="qa__q"> Why are you doing onga /</span> {answers.onga}
+      </p>
+      <p>
+        <span className="qa__q">What are you doing when you are not doing something? /</span> {answers.whatAreYouDoing}
+      </p>
+      <p>
+        <span className="qa__q">A word as Way Too Broad /</span> {answers.aWord}
+      </p>
+    </div>
+  )
+}
