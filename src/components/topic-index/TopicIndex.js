67bb2a379725abf67807675e67ffec521a7367e6
Erin Brown <emberbrown@gmail.com>
2019-06-09 22:44:59 -0400

    Add chronological sorting in the topic index cuz I'm good like that

--- src/components/topic-index/TopicIndex.js
+++ src/components/topic-index/TopicIndex.js
@@ -3,7 +3,11 @@ import { HashLink as Link } from 'react-router-hash-link';
 import Nav from 'components/nav/Nav';
 import Header from 'components/header/Header';
 import Subheader from 'components/header/subheader/Subheader';
-import { orderedTopicsByLetter, topicCount } from './topicHelper';
+import {
+  orderedTopicsByLetter,
+  topicCount,
+  chronologicalTopicsByDate,
+} from './topicHelper';
 import './TopicIndex.scss';
 
 export default class TopicIndex extends Component {
@@ -12,20 +16,41 @@ export default class TopicIndex extends Component {
     this.changeOrder = this.changeOrder.bind(this);
 
     this.state = {
-      order: 'asc'
+      order: localStorage.getItem('order') || 'asc',
+      view: localStorage.getItem('view') || 'alphabetical',
     };
   }
 
   changeOrder(direction) {
     return () => {
+      localStorage.setItem('order', direction);
       this.setState({
         order: direction
       })
     }
   }
 
+  changeView(view) {
+    return () => {
+      localStorage.setItem('view', view);
+      this.setState({ view })
+    }
+  }
+
+  orderedPodcastYears(obj) {
+    const keys = Object.keys(obj);
+
+    if (this.state.order === 'desc') {
+      return keys;
+    }
+
+    return keys.reverse();
+  }
+
   render() {
     const topicsByLetter = orderedTopicsByLetter(this.props.episodes, this.state.order);
+    const topicsByDate = chronologicalTopicsByDate(this.props.episodes);
+
     return (
       <div>
         <header>
@@ -33,38 +58,117 @@ export default class TopicIndex extends Component {
         </header>
         <Subheader text={`We've talked about ${topicCount(this.props.episodes)} things!`} />
         <Nav />
+        <div className="order-view">
+          <div
+            className={`order-view-link ${this.state.view === 'alphabetical' && 'order-view__button--selected'}`}
+            onClick={this.changeView('alphabetical')}
+          >
+            Alphabetical order
+          </div>
+          <div
+            className={`order-view-link ${this.state.view === 'chronological' && 'order-view__button--selected'}`}
+            onClick={this.changeView('chronological')}
+          >
+            Chronological order
+          </div>
+        </div>
 
         <div className="sort">
-          <div className="order">
-            <i className={`${this.state.order === 'asc' && 'order__button--selected'} order__button asc fas fa-arrow-circle-up`} onClick={this.changeOrder('asc')}></i>
-            <i className={`${this.state.order === 'desc' && 'order__button--selected'} order__button desc fas fa-arrow-circle-down`} onClick={this.changeOrder('desc')}></i>
-          </div>
-          <div className="alphabet-search">
-            {
-              Object.keys(topicsByLetter).map(letter => <Link className="alphabet-search__link" to={`/index#${letter}`}>{letter}</Link>)
-            }
-          </div>
+          {
+            (this.state.view == 'alphabetical')
+              ? (
+                <React.Fragment>
+                  <div className="order">
+                    <i className={`${this.state.order === 'asc' && 'order__button--selected'} order__button asc fas fa-arrow-circle-up`} onClick={this.changeOrder('asc')}></i>
+                    <i className={`${this.state.order === 'desc' && 'order__button--selected'} order__button desc fas fa-arrow-circle-down`} onClick={this.changeOrder('desc')}></i>
+                  </div>
+                  <div className="alphabet-search">
+                    {
+                      Object.keys(topicsByLetter).map(letter => <Link key={letter} className="alphabet-search__link" to={`/index#${letter}`}>{letter}</Link>)
+                    }
+                  </div>
+                </React.Fragment>
+              )
+              : (
+                <React.Fragment>
+                  <div className="order">
+                    <i className={`${this.state.order === 'asc' && 'order__button--selected'} order__button asc fas fa-arrow-circle-up`} onClick={this.changeOrder('asc')}></i>
+                    <i className={`${this.state.order === 'desc' && 'order__button--selected'} order__button desc fas fa-arrow-circle-down`} onClick={this.changeOrder('desc')}></i>
+                  </div>
+                  <div className="chronological-search">
+                    {
+                      Object.keys(topicsByDate).map((year) => (
+                        <div key={year}>
+                          <Link to={`/index#${year}`}>
+                            <h3>{year}</h3>
+                          </Link>
+                          <div className="chrono-months">
+                            {
+                              Object.keys(topicsByDate[year]).map((month) => (
+                                <Link key={month} className="alphabet-search__link" to={`/index#${year}-${month}`}>
+                                  {month}
+                                </Link>
+                              ))
+                            }
+                          </div>
+                        </div>
+                      ))
+                    }
+                  </div>
+                </React.Fragment>
+              )
+          }
         </div>
 
         <div className="topics">
           <div id="top"></div>
           {
-            Object.keys(topicsByLetter).map((key) => {
-              return (
-                <div>
-                  <div id={key}>
-                    {key}
-                  </div>
-                  {
-                    topicsByLetter[key].map((topicOb, index) => (
-                      <div className="topics__item">
-                        <Link key={index} className="topic-link" to={`/episodes#${topicOb.episodeNumber}`}>{topicOb.topic}</Link>
+            this.state.view === 'alphabetical'
+              ? (
+                Object.keys(topicsByLetter).map((key) => {
+                  return (
+                    <div key={key}>
+                      <div id={key}>
+                        {key}
                       </div>
-                    ))
-                  }
-                </div>
+                      {
+                        topicsByLetter[key].map((topicOb, index) => (
+                          <div className="topics__item" key={index}>
+                            <Link className="topic-link" to={`/episodes#${topicOb.episodeNumber}`}>{topicOb.topic}</Link>
+                          </div>
+                        ))
+                      }
+                    </div>
+                  )
+                })
               )
-            })
+              : (
+                this.orderedPodcastYears(topicsByDate).map((year) => {
+                  return (
+                    <div key={year}>
+                      <h3 id={year}>
+                        {year}
+                      </h3>
+                      {
+                        Object.keys(topicsByDate[year]).map((month) => (
+                          <React.Fragment key={month}>
+                            <div id={`${year}-${month}`}>
+                              <b>{month}</b>
+                            </div>
+                            {
+                              topicsByDate[year][month].map((topicOb, index) => (
+                                <div className="topics__item" key={index}>
+                                  <Link className="topic-link" to={`/episodes#${topicOb.episodeNumber}`}>{topicOb.topic}</Link>
+                                </div>
+                              ))
+                            }
+                          </React.Fragment>
+                        ))
+                      }
+                    </div>
+                  )
+                })
+              )
           }
           <div id="bottom"></div>
         </div>
--- src/components/topic-index/topicHelper.js
+++ src/components/topic-index/topicHelper.js
@@ -1,18 +1,23 @@
 import { getEpisodeNumberFromTitle, getFirstLetter } from '../../titleHelper';
 
-export function orderedTopicsWithEpisodes(episodes, order = 'asc') {
-  const ob = {};
+function topicsWithEpisodeNumbers(episodes) {
   return [].concat.apply([], episodes.map((ep) => {
     const subtitle = ep.itunes.subtitle;
     const topicsString = 'Topics: ';
     if (subtitle.includes(topicsString) && ep.enclosure) {
       const episodeNumber = getEpisodeNumberFromTitle(ep.title);
+      const date = ep.isoDate;
       return subtitle.split(topicsString)[1].split(', ').map(topic => (
-        { topic, episodeNumber }
+        { topic, episodeNumber, date }
       ))
     }
     return null;
-  }).filter(ob => ob)).sort((a, b) => {
+  })
+    .filter(ob => ob));
+}
+
+export function orderedTopicsWithEpisodes(episodes, order = 'asc') {
+  return topicsWithEpisodeNumbers(episodes).sort((a, b) => {
     const topicA = a.topic.toUpperCase().trim();
     const topicB = b.topic.toUpperCase().trim();
 
@@ -28,6 +33,41 @@ export function orderedTopicsWithEpisodes(episodes, order = 'asc') {
   });
 }
 
+export function chronologicalTopicsByDate(episodes) {
+  const ob = {};
+  const monthMap = {
+    '01': 'January',
+    '02': 'February',
+    '03': 'March',
+    '04': 'April',
+    '05': 'May',
+    '06': 'June',
+    '07': 'July',
+    '08': 'August',
+    '09': 'September',
+    '10': 'October',
+    '11': 'November',
+    '12': 'December'
+  };
+
+  topicsWithEpisodeNumbers(episodes).reverse().forEach((topicOb) => {
+    const year = topicOb.date.substring(0, 4);
+    const month = monthMap[topicOb.date.substring(5, 7)];
+
+    if (!ob[year]) {
+      ob[year] = {};
+    }
+
+    if (!ob[year][month]) {
+      ob[year][month] = [];
+    }
+
+    ob[year][month].push(topicOb);
+  });
+
+  return ob;
+}
+
 export function orderedTopicsByLetter(episodes, order = 'asc') {
   const ob = {};
   orderedTopicsWithEpisodes(episodes, order).forEach((topicOb) => {
@@ -40,6 +80,7 @@ export function orderedTopicsByLetter(episodes, order = 'asc') {
 
   return ob;
 }
+
 
 export function topicCount(episodes) {
   return orderedTopicsWithEpisodes(episodes).length;
